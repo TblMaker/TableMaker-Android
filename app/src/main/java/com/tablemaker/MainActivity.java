d1da1962c370114349665043d240773670892bc8
jangsuhwan <baseball9185@naver.com>
2018-04-09 16:20:26 +0900

    application name change

--- app/src/main/java/com/tablemaker/MainActivity.java
+++ app/src/main/java/com/tablemaker/MainActivity.java
@@ -1,7 +1,9 @@
-package arg.androidtown.myapplication;
+package com.tablemaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tablemaker.R;
 
 public class MainActivity extends AppCompatActivity {
 
